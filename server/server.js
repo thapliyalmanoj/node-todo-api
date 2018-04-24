1e02c4e8286b82486bbcc6a4714bebb490a15a2b
Manoj Thapliyal <mnjthapliyal@gmail.com>
2018-04-24 21:54:46 +0530

    Add GET/ Todos route

--- server/server.js
+++ server/server.js
@@ -25,6 +25,14 @@ res.status(400). send(e);
 
 });
 
+app.get('/todos',(req, res) => {
+	Todo.find().then( (todos) =>{
+      res.send({todos});
+	}, (e) =>{
+      res.status(400).send(e);
+	})
+});
+
 app.listen(3000, () => {
 console.log('started on port 3000');
 });
