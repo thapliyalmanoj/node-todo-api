034ab9d1865ea83147808e452f4764c133227a18
Manoj Thapliyal <mnjthapliyal@gmail.com>
2018-04-28 15:30:22 +0530

    Add DELETE /todos/:id route

--- server/server.js
+++ server/server.js
@@ -58,6 +58,26 @@ if(!ObjectID.isValid(id) ){
    
 });
 
+
+app.delete('/todos/:id', (req, res) =>{
+var id = req.params.id;
+
+if(!ObjectID.isValid(id)) {
+	return res.status(404).send();
+}
+
+
+Todo.findByIdAndRemove(id).then( (todo) => {
+	if(!todo) {
+		return res.status(404).send();
+	}
+ 
+ res.send(todo);
+}).catch((e) => {
+	res.status(400).send();
+});
+});
+
 app.listen(port, () => {
 console.log(`Started at port ${port}`);
 });
